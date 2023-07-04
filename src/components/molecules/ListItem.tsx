40dfe68703e95f0efbdcb2333053b3c6219dbb12
yoyongjin <yongjinalize@gmail.com>
2023-07-04 15:13:01 +0900

    Feat: Add checkbox

--- src/components/molecules/ListItem.tsx
+++ src/components/molecules/ListItem.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../atoms/Button";
 import { Todo } from "../../types";
 import styled from "styled-components";
+import Input from "../atoms/Input";
 
 const Container = styled.div`
   display: flex;
@@ -11,11 +12,27 @@ const Container = styled.div`
   padding: 1rem;
 `;
 
+const TodoStateBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const ListItem = (todo: Todo) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const onCheckHandler = () => {
+    setIsChecked((prev) => !prev);
+  };
+  console.log("isChecked of", todo.text, "is", isChecked);
   return (
     <Container>
       <span>{todo.text}</span>
-      <Button>DEL</Button>
+      <TodoStateBox>
+        <Input
+          type="checkbox"
+          onChange={onCheckHandler}
+        />
+        <Button>DEL</Button>
+      </TodoStateBox>
     </Container>
   );
 };
